80a0725cfb98da587790cc7ece8ede330a5f0742
manon <manon.faucher.pro@gmail.com>
2021-04-07 11:24:40 +0200

    firt commit, initialize project properties

---
+++ src/main/java/fr/udara/UdaraApplication.java
@@ -0,0 +1,13 @@
+package fr.udara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/udara/UdaraApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.udara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
